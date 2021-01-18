5ccb23ac702882f0db4f1ddd4473ce07c751e01b
Chris Gambrell <christopher@gambrell.info>
2021-01-18 12:59:02 -0500

    can query users by string using /user?displayName=foo

--- api/functions/routes/user/getUser.js
+++ api/functions/routes/user/getUser.js
@@ -1,3 +1,5 @@
+const { db } = require('../../util/config')
+
 module.exports = (req, res) => {
 	if (req.params.uid) {
 		// fetch user from uid
@@ -5,6 +7,28 @@ module.exports = (req, res) => {
 			code: 'general: todo',
 			message: 'This function is not yet completed',
 		})
+	} else if (req.query.displayName) {
+		db.collection('users')
+			.where('displayName', '>=', req.query.displayName)
+			.where(
+				'displayName',
+				'<',
+				String.fromCharCode(req.query.displayName.charCodeAt(0) + 1)
+			)
+			.get()
+			.then((snapshot) => {
+				var users = []
+				users = snapshot.docs.map((document) => ({
+					id: document.id,
+					...document.data(),
+				}))
+
+				return res.json(users)
+			})
+			.catch((error) => {
+				console.error(error)
+				return res.status(500).json(error)
+			})
 	} else {
 		return res.json(req.user)
 	}
